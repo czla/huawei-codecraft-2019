1917e9c53c12ff40eb95781037c56972e6584b2b
zlchen <zlchen@tongji.edu.cn>
2019-03-29 23:44:00 +0800

    modify matrix

--- SDK/SDK_python/CodeCraft-2019/src/debug_czl.py
+++ SDK/SDK_python/CodeCraft-2019/src/debug_czl.py
@@ -1,11 +1,11 @@
 import numpy as np
 
-original_time = np.linspace(1, 10, dtype=int)
+car_array = np.ones((5, 5))
 
-print(original_time)
+print(car_array)
 
-start_time = original_time.copy()
-for i in range(len(original_time)):
+#start_time = car_array.copy()
+for i in range(car_array.shape[0]):
     delay_chance = np.random.uniform(0, 1)
     if delay_chance < 0.2:
         delay_time = np.random.randint(1, 60)
@@ -15,6 +15,18 @@ for i in range(len(original_time)):
         delay_time = np.random.randint(110, 230)
     else:
         delay_time = np.random.randint(230, 350)
-    start_time[i] += delay_time
+    car_array[i, -1] += delay_time
 
-print(start_time)
+print(car_array)
+car_array = car_array[car_array[:, -1].argsort()] #按照第3列对行排序
+
+print(car_array)
+
+Matrix_slow = -1 * np.ones((car_array.shape[0], car_array.shape[0]), dtype=int)
+Matrix_normal = Matrix_slow.copy()
+Matrix_fast = Matrix_slow.copy()
+
+print(Matrix_fast)
+print(Matrix_slow)
+print(Matrix_normal)
+
